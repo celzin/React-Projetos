ebd36b3d6d089fb30b93d67488ad2fdca0b4f419
Celso WSL <celsovinicius4@gmail.com>
2024-11-11 17:25:49 -0300

    feat[hierarquia de componentes]: testando estrutura de hierarquia

---
+++ 2_Fundamentos/fundamentos/src/components/MyComponent.jsx
@@ -0,0 +1,9 @@
+const MyComponent = () => {
+    return (
+        <div>
+            <h2>Este componente está sendo reaproveitado em vários lugares!</h2>
+        </div>
+    )
+}
+
+export default MyComponent
