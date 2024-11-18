59a3be880ac737cf05ba7ae296c3eb7be0e087ff
Celso WSL <celsovinicius4@gmail.com>
2024-11-18 20:12:34 -0300

    feat[key]: aplicando a propriedade key

--- 3_Avancando_Em_React/avancando/src/components/ListRender.js
+++ 3_Avancando_Em_React/avancando/src/components/ListRender.js
@@ -1,13 +1,24 @@
 import { useState } from "react"
 
 const ListRender = () => {
-  const [list] = useState(["Celso", "Pedro", "Luan"])
-  
+  const [list] = useState(["Celso", "Pedro", "Luan", "Marcus"])
+  const [users] = useState([
+    {id: 1, name: "Celso", age: 22},
+    {id: 2345, name: "Pedro", age: 24},
+    {id: 6789, name: "Luan", age: 20},
+  ])
     return (
     <div>
         <ul>
-            {list.map((item) => (
-                <li>{item}</li>
+            {list.map((item, i) => (
+                <li key={i}>{item}</li>
+            ))}
+        </ul>
+        <ul>
+            {users.map((user) => (
+                <li key={user.id}>
+                    {user.name}, {user.age}
+                </li>
             ))}
         </ul>
     </div>
