a6942151c596dd09b9711981d91ccdee79acf08b
Celso WSL <celsovinicius4@gmail.com>
2024-11-18 20:20:52 -0300

    feat[previusState]:  deletando usuários apartir de um filtro

--- 3_Avancando_Em_React/avancando/src/components/ListRender.js
+++ 3_Avancando_Em_React/avancando/src/components/ListRender.js
@@ -2,11 +2,21 @@ import { useState } from "react"
 
 const ListRender = () => {
   const [list] = useState(["Celso", "Pedro", "Luan", "Marcus"])
-  const [users] = useState([
+  
+  const [users, setUsers] = useState([
     {id: 1, name: "Celso", age: 22},
-    {id: 2345, name: "Pedro", age: 24},
-    {id: 6789, name: "Luan", age: 20},
+    {id: 2, name: "Pedro", age: 24},
+    {id: 3, name: "Luan", age: 20},
   ])
+
+  const deleteRandom = () => {
+    const randomNumber = Math.floor(Math.random() * 4)
+
+    setUsers((prevUsers) => {
+        return prevUsers.filter((user) => randomNumber !== user.id)
+    })
+  } 
+
     return (
     <div>
         <ul>
@@ -21,6 +31,7 @@ const ListRender = () => {
                 </li>
             ))}
         </ul>
+        <button onClick={deleteRandom}>Delete random user</button>
     </div>
   )
 }
