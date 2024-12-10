e341b726e4671e6189820c7cebf908b24831fbb2
Celso WSL <celsovinicius4@gmail.com>
2024-12-09 21:51:02 -0300

    feat[forms]: gerenciamento de dados input

--- 5_Forms_Em_React/forms/src/components/MyForm.js
+++ 5_Forms_Em_React/forms/src/components/MyForm.js
@@ -1,13 +1,24 @@
 import "./MyForm.css"
+import { useState } from 'react'
 
 function MyForm() {
+    // 3 - gerenciamento de dados input
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const handleName = (e) => {
+        // console.log(e.target.value)
+        setName(e.target.value)
+    }
+    console.log(name)
+    
+
   return (
     <div>
         {/* 1 - criacao de form */}
         <form action="">
             <div>
                 <label htmlFor="name">Nome: </label>
-                <input type="text" name='name' placeholder='Digite o seu nome' />
+                <input type="text" name='name' placeholder='Digite o seu nome' onChange={handleName} />
             </div>
             <input type="submit" value="enviar" />
             {/* 2 - label envolvendo input */}
