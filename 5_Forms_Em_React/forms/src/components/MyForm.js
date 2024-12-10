0fe3e0423d0bb8350399e86e691aca7d3f0a04f1
Celso WSL <celsovinicius4@gmail.com>
2024-12-09 21:56:21 -0300

    refactor[state]: alterando state diretamente na linha

--- 5_Forms_Em_React/forms/src/components/MyForm.js
+++ 5_Forms_Em_React/forms/src/components/MyForm.js
@@ -10,7 +10,7 @@ function MyForm() {
         setName(e.target.value)
     }
     console.log(name)
-    
+    console.log(email)
 
   return (
     <div>
@@ -24,7 +24,7 @@ function MyForm() {
             {/* 2 - label envolvendo input */}
             <label>
                 <span>E-mail:</span>
-                <input type='email' name='email' placeholder="Digite o seu e-mail" />
+                <input type='email' name='email' placeholder="Digite o seu e-mail" onChange={(e) => setEmail(e.target.value)}/>
             </label>
         </form>
     </div>
