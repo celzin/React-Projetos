65373ee1b0e2e14af5b411dbe6e08cb32d39ac21
Celso WSL <celsovinicius4@gmail.com>
2024-12-09 22:02:08 -0300

    feat[forms]: envio de formulario pelo onSubmit e prevenindo page reload

--- 5_Forms_Em_React/forms/src/components/MyForm.js
+++ 5_Forms_Em_React/forms/src/components/MyForm.js
@@ -9,13 +9,20 @@ function MyForm() {
         // console.log(e.target.value)
         setName(e.target.value)
     }
-    console.log(name)
-    console.log(email)
+    // console.log(name)
+    // console.log(email)
+    
+    const handleSubmit = (event) => {
+        event.preventDefault(); // Evita o page reload
+        console.log("Enviando o formulário")
+        console.log(name, email);
+    };
 
   return (
     <div>
+        {/* 5 - envio de form */}
         {/* 1 - criacao de form */}
-        <form action="">
+        <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">Nome: </label>
                 <input type="text" name='name' placeholder='Digite o seu nome' onChange={handleName} />
@@ -24,6 +31,7 @@ function MyForm() {
             {/* 2 - label envolvendo input */}
             <label>
                 <span>E-mail:</span>
+                {/* 4 - simplificação de manipulação de state */}
                 <input type='email' name='email' placeholder="Digite o seu e-mail" onChange={(e) => setEmail(e.target.value)}/>
             </label>
         </form>
